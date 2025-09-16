0c548e97ed5fb4ffa8da93278f52dc219188ad8d
xgaslann <gorkem_aslan0@hotmail.com>
2025-09-16 22:54:06 +0300

    xgaslan initial

---
+++ src/main/java/com/xgaslan/orderservice/EdaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xgaslan.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdaOrderServiceApplication.class, args);
+    }
+
+}
